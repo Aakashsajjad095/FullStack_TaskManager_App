aa4209f544d559e3d20996dd3ab3b0c21752283c
Aakash <dplaakash@gmail.com>
2025-03-05 01:17:42 +0500

    Remove unnecessary code and clean up the project

--- TaskManager_Backend/src/server.js
+++ TaskManager_Backend/src/server.js
@@ -1,18 +1,3 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import taskRoutes from './routes/taskRoutes.js';
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(bodyParser.json());
-// app.use('/api', taskRoutes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
 import express from 'express';
 import cors from 'cors';  // Import CORS package
 import taskRoutes from './routes/taskRoutes.js';
